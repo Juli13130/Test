7618f0c174c8b260ad12b69539cb2c9436c8a649
Juli <Julian1313000@gmail.com>
2024-04-28 03:50:45 -0300

    update pruebas.py and test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import getpass
+import sys ## parte de limpiar_consola(), se puede eliminar
+loop = True # esto no puede cambiar ya que es preferible salir mediante un exit controlado
 sesion_iniciada = False
 usuarios = {
     "estudiante1@ayed.com": "111222",
@@ -17,9 +19,10 @@ def menu_inicio():
         registro()
     elif seleccion_inicio == "1":
         login()
-    else:
+    else:       ##no estoy seguro si es preferible a un if
         exit("Saliendo...")
 def registro():
+    global sesion_iniciada
     print("codigo va aca")
     sesion_iniciada = True
 def login():
@@ -43,13 +46,64 @@ def login():
                 print("Correo electronico incorrecto. Ingrese nuevamente su correo:")
             else:
                 print("Ha intentado el maximo de intentos permitidos. Fin del Inicio de Sesion")
-while sesion_iniciada is False:
-    menu_inicio()
+
+def limpiar_consola(n=1):     ### mas que nada para limpiar la consola de menus previos, se puede eliminar sin problemas
+    for _ in range(n):
+        sys.stdout.write("\x1b[1A")  # cursor up one line
+        sys.stdout.write("\x1b[2K")  # delete the last line
+
+def editor_datos():
+    print("yay")
 def menu_principal():
+    global sesion_iniciada
     print(" ")
     print("0-Salir")
     print("1-Gestionar mi perfil")
     print("2-Gestionar candidatos")
     print("3-Matcheos")
     print("4-Reportes estadisticos")
-menu_principal()
+    seleccion_pri = input("Selecionar: >")
+    if seleccion_pri == "1":
+        limpiar_consola(5)
+        print("--Gestionar mi perfil--")
+        print("a-Editar mis datos personales")
+        if input("sel: ").casefold() == "a":
+            editor_datos()
+        else:
+            return()
+        ###print("b-")
+        ###print("c-Salir")
+    elif seleccion_pri == "2":
+        limpiar_consola(6)
+        print("--Gestionar candidatos--")
+        print("a-Ver candidatos")
+        ###print("b-")
+        ###print("c-Salir")
+    elif seleccion_pri == "3":
+        limpiar_consola(6)
+        print("--Matcheos--")
+        print("a-Editar mis datos personales")
+        print("b-")
+        print("c-Salir")
+    elif seleccion_pri == "4":
+        print("en construccion")
+    elif seleccion_pri == "0":
+        limpiar_consola(6)
+        print("0-Volver")
+        print("1-Salir de la cuenta")
+        print("~-Salir")
+        salir = input("Estas seguro? ")
+        if salir == "0":
+            return
+        elif salir == "1":
+            sesion_iniciada = False
+        else:
+            exit
+    else:
+        print("?")
+
+while loop == True:         #mantiene al usuario en un menu u otro
+    if sesion_iniciada == False:
+        menu_inicio()
+    if sesion_iniciada == True:
+        menu_principal()
