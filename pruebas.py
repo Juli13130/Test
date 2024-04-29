76380bfcd4c2c5586a03a8bbe8ceca1c65d197c4
Juli <Julian1313000@gmail.com>
2024-04-28 21:13:14 -0300

    update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,7 +1,11 @@
 import sys
-def delete_multiple_lines(n=1):
-    """Delete the last line in the STDOUT."""
+def limpiar(n=1):
     for _ in range(n):
         sys.stdout.write("\x1b[1A")  # cursor up one line
         sys.stdout.write("\x1b[2K")  # delete the last line
 
+
+num = range(1,11)  # hace 10 lineas
+for n in num:
+    print(n)
+limpiar(5)  # limpia (5) lineas
